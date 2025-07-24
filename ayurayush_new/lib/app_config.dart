23d085bda46faf4f2beba29715f9cf23bb6e02e1
ARAVIND MURALI T <aravind.Thricovil@gmail.com>
2025-07-24 17:45:09 -0400

    Update app_config.dart

--- ayurayush_new/lib/app_config.dart
+++ ayurayush_new/lib/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   // static const String apiBaseUrl = 'http://104.211.241.125/api';
-  static const String apiBaseUrl = 'http://0.0.0.0:8080/api';
+  static const String apiBaseUrl = 'http://4.247.29.42/api';
 }
